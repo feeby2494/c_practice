cdeac74ca838d2ea47fb6db6500025ac810dab1b
feeby2494 <toby2494@hotmail.com>
2022-12-24 02:11:58 -0600

    added story with varables.

---
+++ story.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int age = 35;
+    char name[] = "John";
+
+    printf("There once was a man named %s\n", name);
+    printf("he was %d years old.\n", age);
+    printf("He really liked the name %s\n", name);
+    printf("but did not like being %d.\n", age);
+
+    return 0;
+}
