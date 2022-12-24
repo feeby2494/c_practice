e3a6428f184473569f2d5cd459a4a9ee57a4194d
feeby2494 <toby2494@hotmail.com>
2022-12-24 01:49:29 -0600

    Made hello and triangle programs.

---
+++ triangle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("     /\\\n");
+    printf("    /  \\\n");
+    printf("   /    \\\n");
+    printf("  /      \\\n");
+    printf(" /        \\\n");
+    printf("/__________\\\n");
+
+    return 0;
+}
